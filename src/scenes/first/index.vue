bbdc2b3bffb80abc3aacf5212aaaae4930643a22
lafeillou <lafeillou@126.com>
2019-08-05 17:19:38 +0800

    init repo

--- src/scenes/first/index.vue
+++ src/scenes/first/index.vue
@@ -3,20 +3,79 @@
 </template>
 
 <script>
+import { resize } from "@/utils/utils.js";
+import _ from "lodash";
+
 export default {
   data() {
-    return {};
+    return {
+      letters: {},
+      lettersPos: {
+        a: {
+          url: "/assets/images/letters/a.png",
+          w: 173,
+          h: 173,
+          x: 276,
+          y: 149
+        },
+        b: {
+          url: "/assets/images/letters/b.png",
+          w: 173,
+          h: 173,
+          x: 474,
+          y: 149
+        },
+        c: {
+          url: "/assets/images/letters/c.png",
+          w: 173,
+          h: 173,
+          x: 680,
+          y: 149
+        },
+        d: {
+          url: "/assets/images/letters/d.png",
+          w: 173,
+          h: 173,
+          x: 908,
+          y: 149
+        },
+        e: {
+          url: "/assets/images/letters/e.png",
+          w: 173,
+          h: 173,
+          x: 367,
+          y: 375
+        },
+        f: {
+          url: "/assets/images/letters/f.png",
+          w: 173,
+          h: 173,
+          x: 575,
+          y: 375
+        },
+        g: {
+          url: "/assets/images/letters/g.png",
+          w: 173,
+          h: 173,
+          x: 800,
+          y: 375
+        }
+      }
+    };
   },
   mounted() {
-    var s = window.Snap("#svg");
-    // Lets create big circle in the middle:
-    var bigCircle = s.circle(150, 150, 100);
-    // By default its black, lets change its attributes
-    bigCircle.attr({
-      fill: "#bada55",
-      stroke: "#000",
-      strokeWidth: 5
+    resize();
+    let s = window.Snap("#svg");
+    _.forEach(this.lettersPos, (value, key) => {
+      this.letters[key] = s.image(
+        value.url,
+        resize(value.x),
+        resize(value.y),
+        resize(value.w),
+        resize(value.h)
+      );
     });
+    console.log(this.letters);
   }
 };
 </script>
---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+import $ from "jquery";
+
+export function resize(value) {
+  console.log($(window).width());
+  let $window = $(window);
+  let phoneWidth = $window.width();
+  // let phoneHeight = $window.height();
+  let radio = phoneWidth / 1334;
+  return value * radio;
+}
