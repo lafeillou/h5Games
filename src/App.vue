65c3c33bc53ca65f8af51bdb82fbae68ee53fb37
lafeillou <lafeillou@126.com>
2019-08-05 15:36:36 +0800

    repo init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">123</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  components: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" >
+@import "./assets/sass/mixin.scss";
+#app {
+  height: 100%;
+  position: relative;
+}
+.main-search-input-wrap {
+  position: absolute;
+  left: 0;
+  @include px2rem(top, 59);
+  width: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import App from "./App.vue";
+// 全局 reset
+import "normalize.css/normalize.css";
+// 全局样式
+import "./assets/sass/main.scss";
+// 淘宝界面元素显示尺寸兼容方案(比较旧，最新推荐vw)
+// 新方案推荐阅读 https://www.w3cplus.com/mobile/vw-layout-in-vue.html
+import "lib-flexible";
+
+Vue.config.productionTip = false;
+new Vue({
+  render: h => h(App),
+  data: {
+    eventHub: new Vue()
+  }
+}).$mount("#app");
